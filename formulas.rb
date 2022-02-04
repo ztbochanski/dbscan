04a2fa603db5ae701dbb00d80d1d51f1d86bb012
zach <ztbochanski@gmail.com>
2022-02-04 14:32:13 -0800

    add wrapper for distance measures gem
    haversine and euclidean wrapped

---
+++ formulas.rb
@@ -0,0 +1,17 @@
+require "distance_measures"
+
+class Euclidean
+
+  def measure(point_p, point_q)
+    point_p.euclidean_distance(point_q)
+  end
+
+end
+
+class Haversine
+
+  def measure(point_p, point_q)
+    point_p.haversine_distance(point_q)
+  end
+  
+end
