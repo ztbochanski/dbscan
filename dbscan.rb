c6866f0084564bdf65094221bcf73b32e0a31acf
zach <ztbochanski@gmail.com>
2022-02-04 16:25:03 -0800

    add conditionals based on number of neighbors

--- dbscan.rb
+++ dbscan.rb
@@ -3,6 +3,7 @@ require "./point.rb"
 require "./spatial_data.rb"
 require "./proximity_scanner.rb"
 
+# test points
 x1 = Point.new(dimensions: [0,0], name: "x1")
 x2 = Point.new(dimensions: [1,0], name: "x2")
 x3 = Point.new(dimensions: [1,1], name: "x3")
@@ -12,10 +13,28 @@ x6 = Point.new(dimensions: [3,0], name: "x6")
 x7 = Point.new(dimensions: [0,1], name: "x7")
 x8 = Point.new(dimensions: [3,2], name: "x8")
 x9 = Point.new(dimensions: [6,3], name: "x9")
+
+# test data
 data = SpatialData.new(points: [x1, x2, x3, x4, x5, x6, x7, x8, x9])
 
+# inputs
+NEIGHBOR_RANGE = 1
+CORE_POINT_DENSITY_THRESHOLD = 3
+
+# dbscan algorithm
 data.points.each do | point |
-  prox_scanner = ProximityScanner.new(data: data, point_p: point, neighbor_range: 1, ranging_method: Euclidean.new)
-  neighbors = prox_scanner.scan_for_neighbors
-  puts "Point #{point.name}: neighbors are: #{neighbors}"
+  neighbors = ProximityScanner.new(data: data, 
+                                   point_p: point, 
+                                   neighbor_range: NEIGHBOR_RANGE, 
+                                   ranging_method: Euclidean.new
+                                   ).scan_for_neighbors
+
+  if neighbors.size >= CORE_POINT_DENSITY_THRESHOLD
+    puts "core #{point.name}: neighbors are: #{neighbors}"
+  elsif neighbors.size == 1
+    puts "noise #{point.name}: neighbors are: #{neighbors}"
+  else
+    puts "border #{point.name}: neighbors are: #{neighbors}"
+  end
+
 end
--- range_finder.rb
+++
@@ -1,7 +0,0 @@
-class RangeFinder
-
-  def measure(ranging_method, point_p, point_q)
-    ranging_method.measure(point_p, point_q)
-  end
-
-end
